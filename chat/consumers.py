972a15dce8a9cbc7638643e713d68dc6c745665a
Murad <muradmustafayev03@gmail.com>
2023-01-19 18:29:52 +0400

    Initial commit

---
+++ chat/consumers.py
@@ -0,0 +1,34 @@
+import json
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
+
+
+class ChatConsumer(AsyncJsonWebsocketConsumer):
+    async def connect(self):
+        self.groupname = 'dashboard'
+        await self.channel_layer.group_add(
+            self.groupname,
+            self.channel_name,
+        )
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.groupname,
+            self.channel_layer
+        )
+
+    async def receive(self, text_data=None, **kwargs):
+        text_data_json = json.loads(text_data)
+        message = text_data_json["message"]
+        username = text_data_json["username"]
+        await self.channel_layer.group_send(
+            self.groupname, {
+                "type": "sendMessage",
+                "message": message,
+                "username": username,
+            })
+
+    async def sendMessage(self, event):
+        message = event["message"]
+        username = event["username"]
+        await self.send(text_data=json.dumps({"message": message, "username": username}))
